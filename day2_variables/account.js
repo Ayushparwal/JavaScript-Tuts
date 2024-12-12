5a393595295e9a42a17488c6667052a751d0284a
Ayush Parwal <133211165+Ayushparwal@users.noreply.github.com>
2024-12-12 19:17:57 +0530

    Add files via upload

---
+++ day2_variables/account.js
@@ -0,0 +1,14 @@
+const accountid = 545
+let accountEmail = "ayush@google.com"
+var passwordOfEmail = "124578"
+accountcity = "Nagpur"
+
+// accountid = 245 not allowed 
+console.log(accountid);
+accountEmail = "hi@gmail.com"
+accountcity = "nanded"
+
+console.table([accountEmail,accountid,passwordOfEmail,accountcity])
+
+/* prefer not to use var */ 
+
---
+++ day3_datatypes/datatypes.js
@@ -0,0 +1,22 @@
+let name = "ayush"
+let age = 20
+let isLoggedin = true
+let state =null
+
+
+// number 
+// string
+// bigint
+// boolean
+// null ==> standalone value
+// undefined
+// symbol ==> used in reactjs
+
+
+// object
+
+console.log(typeof("hindi"));
+console.log(typeof(20));
+console.log(typeof null)
+
+
